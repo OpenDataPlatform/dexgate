73f1b8cfafb15452193d57e5feb5820cc34f873e
SergeAlexandre <sa@broadsoftware.com>
2021-10-27 00:06:58 +0200

    Adding session handling

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"dexgate/internal/config"
 	"dexgate/internal/director"
+	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"net/http/httputil"
@@ -10,20 +12,38 @@ import (
 
 var log *logrus.Entry
 
+var tokenCount int = 0
+
+func newToken() string {
+	tokenCount++
+	return fmt.Sprintf("%05d", tokenCount)
+}
+
 func main() {
 	config.Setup()
 	log = config.GetLog()
 	log.Infof("Dexgate %s listening at '%s' to forward to '%s' (Logleve:%s)", config.GetVersion(), config.GetBindAddr(), config.GetTargetURL(), config.GetLogLevel())
 
+	sessionManager := scs.New()
+
 	reverseProxy := &httputil.ReverseProxy{Director: director.NewDirector(config.GetTargetURL())}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//for name, values := range r.Header {
 		//	for _, value := range values {
 		//		log.Debugf("%s %s", name, value)
 		//	}
 		//}
+		token := sessionManager.GetString(r.Context(), "token")
+		if token == "" {
+			token = newToken()
+			sessionManager.Put(r.Context(), "token", token)
+			log.Debugf("New token:%s", token)
+		} else {
+			log.Debugf("token:%s", token)
+		}
 		reverseProxy.ServeHTTP(w, r)
 	})
-	log.Fatal(http.ListenAndServe(config.GetBindAddr(), nil))
+	log.Fatal(http.ListenAndServe(config.GetBindAddr(), sessionManager.LoadAndSave(mux)))
 }
